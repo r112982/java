36d23d87abccc430b0a1f69d1f086a3a4ac24e65
r112982 <r112982@gmail.com>
2017-12-09 14:26:34 +0800

    no message

---
+++ src/testPt02.java
@@ -0,0 +1,10 @@
+﻿package testST;
+
+public class testPt02 {
+	public static void main(String[] args) {
+		System.out.println("新增點東西");
+System.out.println("新增點東西1");
+System.out.println("新增點東西2");
+
+	}
+}
