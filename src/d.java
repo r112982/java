03046c0c66a654a25dd45f52048c5b9720fe0749
sonicTsai <sonictsailogin@gmail.com>
2017-12-09 14:44:04 +0800

    衝突測試

--- src/d.java
+++ src/d.java
@@ -11,13 +11,12 @@ public class d {
 //sonic testing
 
 //123465465461656516541
-<<<<<<< HEAD
 	//djfajf;ajf;dafkj
  //安安安安安安安安安安安安安安安安安安安安安安
-=======
 		//test
 	 
->>>>>>> 2cff867f003bc03d63bf63107158f58e271be8c8
+// 2cff867f003bc03d63bf63107158f58e271be8c8
+// 不要再打了
 	}
 
 }
